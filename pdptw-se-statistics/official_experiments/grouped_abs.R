a09ef0bff5610c706a3add272dacd50bdf482f5d
vabarbosa <vitor.alves@mindsight.com.br>
2025-12-29 15:51:21 -0300

    feat: grouped abs grb and hx

--- pdptw-se-statistics/official_experiments/grouped_abs.R
+++ pdptw-se-statistics/official_experiments/grouped_abs.R
@@ -1,12 +1,12 @@
 library(dplyr)
 library(knitr)
+library(readr)
 
 solvers <- c("gurobi", "hexaly")
 solvers_suff <- c("", "_hx")
-benchmarks <- c("benchmark_multi_island_v5", "benchmark_multi_floor_v3")
 variations <- c("I5", "F3")
 prefix_csv_output <- "grouped"
-exp_set <- c("set_02", "set_02")
+types <- c(1, 2)
 
 OPTIMAL <- 2
 OPT_HX <- "HxSolutionStatus.OPTIMAL"
@@ -17,28 +17,23 @@ KILLED <- 11
 KILLED_HX <- "KILLED"
 
 for (i in seq_along(solvers)){
-  prefix_set <- paste0("official_experiments/mip_", solvers[i])
+  prefix_set <- paste0("official_experiments/data/mip_", solvers[i])
   prefix_csv_input <- paste0("csvresults_form_melo", solvers_suff[i])
-  for (j in seq_along(benchmarks)) {
-    for (t in 1:2) {
+  for (j in seq_along(variations)) {
+    for (t in seq_along(types)) {
       suff_output <- paste0("abs_type_", t)
-      csv_results_df <-
-        read.csv(
-          file = paste0(
-            "../",
-            benchmarks[j],
-            "/",
-            prefix_set,
-            "/",
-            exp_set[j],
-            "/",
-            prefix_csv_input,
-            "_type_",
-            t,
-            ".csv"
-          ),
-          sep = ";"
-        )
+      csvr_df_file_name <- paste0(prefix_csv_input, "_", variations[j], ".csv")
+      csvr_file_path <- file.path(prefix_set, csvr_df_file_name)
+      csv_results_df <- read_delim(
+        file = csvr_file_path,
+        delim = ";"
+      )
+
+      csv_results_df <- csv_results_df %>%
+        filter(type == paste0("t", t))
+
+      csv_results_df <- csv_results_df %>%
+        rename_with(~ sub("(_grb|_hx)+$", "", .x))
       
       csv_results_df <- csv_results_df %>%
         rename(obj_value = any_of("objValue"))
@@ -62,22 +57,25 @@ for (i in seq_along(solvers)){
           "Feas. Sol." = sum(feas_sol),
         )
       
+      grouped_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_",
+        suff_output,
+        "_",
+        variations[j],
+        ".csv"
+      )
+      grouped_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        grouped_abs_file_name
+      )
+      dir.create(dirname(grouped_abs_file_path), showWarnings = FALSE, recursive = TRUE)
+
       
       write.table(
         grouped_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_",
-          suff_output,
-          ".csv"
-        ),
+        grouped_abs_file_path,
         sep = ";",
         dec = ".",
         quote = F,
@@ -86,22 +84,25 @@ for (i in seq_along(solvers)){
       
       tex_grouped_abs <-
         kable(grouped_abs, format = "latex", booktabs = TRUE)
+
+      tex_grouped_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_",
+        suff_output,
+        "_",
+        variations[j],
+        ".tex"
+      )
+      tex_grouped_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        tex_grouped_abs_file_name
+      )
+      dir.create(dirname(tex_grouped_abs_file_path), showWarnings = FALSE, recursive = TRUE)
       
       writeLines(
         tex_grouped_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_",
-          suff_output,
-          ".tex"
-        )
+        tex_grouped_abs_file_path
       )
       
       ##############################################
@@ -121,22 +122,26 @@ for (i in seq_along(solvers)){
           Killed = sum(killed),
           "Feas. Sol." = sum(feas_sol),
         )
+
+      grouped_by_n_req_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_by_n_req_",
+        suff_output,
+        "_",
+        variations[j],
+        ".csv"
+      )
+      grouped_by_n_req_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        grouped_by_n_req_abs_file_name
+      )
+
+      dir.create(dirname(grouped_by_n_req_abs_file_path), showWarnings = FALSE, recursive = TRUE)
       
       write.table(
         grouped_by_n_req_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_by_n_req_",
-          suff_output,
-          ".csv"
-        ),
+        grouped_by_n_req_abs_file_path,
         sep = ";",
         dec = ".",
         quote = F,
@@ -147,22 +152,25 @@ for (i in seq_along(solvers)){
         kable(grouped_by_n_req_abs,
               format = "latex",
               booktabs = TRUE)
-      
+
+      tex_grouped_by_n_req_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_by_n_req_",
+        suff_output,
+        "_",
+        variations[j],
+        ".tex"
+      )
+      tex_grouped_by_n_req_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        tex_grouped_by_n_req_abs_file_name
+      )
+      dir.create(dirname(tex_grouped_by_n_req_abs_file_path), showWarnings = FALSE, recursive = TRUE)
+
       writeLines(
         tex_grouped_by_n_req_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_by_n_req_",
-          suff_output,
-          ".tex"
-        )
+        tex_grouped_by_n_req_abs_file_path
       )
       
       ##############################################
@@ -182,22 +190,25 @@ for (i in seq_along(solvers)){
           Killed = sum(killed),
           "Feas. Sol." = sum(feas_sol, na.rm = TRUE),
         )
-      
+
+      grouped_by_req_reg_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_by_req_reg_",
+        suff_output,
+        "_",
+        variations[j],
+        ".csv"
+      )
+      grouped_by_req_reg_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        grouped_by_req_reg_abs_file_name
+      )
+      dir.create(dirname(grouped_by_req_reg_abs_file_path), showWarnings = FALSE, recursive = TRUE)
+
       write.table(
         grouped_by_req_reg_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_by_req_reg_",
-          suff_output,
-          ".csv"
-        ),
+        grouped_by_req_reg_abs_file_path,
         sep = ";",
         dec = ".",
         quote = F,
@@ -208,22 +219,25 @@ for (i in seq_along(solvers)){
         kable(grouped_by_req_reg_abs,
               format = "latex",
               booktabs = TRUE)
-      
+
+      tex_grouped_by_req_reg_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_by_req_reg_",
+        suff_output,
+        "_",
+        variations[j],
+        ".tex"
+      )
+      tex_grouped_by_req_reg_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        tex_grouped_by_req_reg_abs_file_name
+      )
+      dir.create(dirname(tex_grouped_by_req_reg_abs_file_path), showWarnings = FALSE, recursive = TRUE)
+
       writeLines(
         tex_grouped_by_req_reg_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_by_req_reg_",
-          suff_output,
-          ".tex"
-        )
+        tex_grouped_by_req_reg_abs_file_path
       )
       
       ##############################################
@@ -245,22 +259,25 @@ for (i in seq_along(solvers)){
           killed = sum(killed),
           feas_sol = sum(feas_sol),
         )
-      
+
+      grouped_by_req_mach_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_by_req_mach_",
+        suff_output,
+        "_",
+        variations[j],
+        ".csv"
+      )
+      grouped_by_req_mach_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        grouped_by_req_mach_abs_file_name
+      )
+      dir.create(dirname(grouped_by_req_mach_abs_file_path), showWarnings = FALSE, recursive = TRUE)
+
       write.table(
         grouped_by_req_mach_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_by_req_mach_",
-          suff_output,
-          ".csv"
-        ),
+        grouped_by_req_mach_abs_file_path,
         sep = ";",
         dec = ".",
         quote = F,
@@ -271,22 +288,25 @@ for (i in seq_along(solvers)){
         kable(grouped_by_req_mach_abs,
               format = "latex",
               booktabs = TRUE)
-      
+
+      tex_grouped_by_req_mach_abs_file_name <- paste0(
+        prefix_csv_output,
+        "_by_req_mach_",
+        suff_output,
+        "_",
+        variations[j],
+        ".tex"
+      )
+      tex_grouped_by_req_mach_abs_file_path <- file.path(
+        prefix_set,
+        "grouped",
+        tex_grouped_by_req_mach_abs_file_name
+      )
+      dir.create(dirname(tex_grouped_by_req_mach_abs_file_path), showWarnings = FALSE, recursive = TRUE)
+
       writeLines(
         tex_grouped_by_req_mach_abs,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_by_req_mach_",
-          suff_output,
-          ".tex"
-        )
+        tex_grouped_by_req_mach_abs_file_path
       )
     }
   }
