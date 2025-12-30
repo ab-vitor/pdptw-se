55d323c39f90d7245a6afc822e6b9e7cced2a816
vabarbosa <vitor.alves@mindsight.com.br>
2025-12-29 23:06:56 -0300

    feat: grouped avrg grb and hx

--- pdptw-se-statistics/official_experiments/grouped_avrg.R
+++ pdptw-se-statistics/official_experiments/grouped_avrg.R
@@ -2,35 +2,29 @@ library(dplyr)
 
 solvers <- c("gurobi", "hexaly")
 solvers_suff <- c("", "_hx")
-benchmarks <- c("benchmark_multi_island_v5", "benchmark_multi_floor_v3")
 variations <- c("I5", "F3")
 prefix_csv_output <- "grouped"
-exp_set <- c("set_02", "set_02")
+types <- c(1, 2)
 
 for (i in seq_along(solvers)){
-  prefix_set <- paste0("official_experiments/mip_", solvers[i])
+  prefix_set <- paste0("official_experiments/data/mip_", solvers[i])
   prefix_csv_input <- paste0("csvresults_form_melo", solvers_suff[i])
-  for (j in seq_along(benchmarks)) {
-    for (t in 1:2) {
+  for (j in seq_along(variations)) {
+    for (t in seq_along(types)) {
       suff_output <- paste0("avrg_type_", t)
-      csv_results_df <-
-        read.csv(
-          file = paste0(
-            "../",
-            benchmarks[j],
-            "/",
-            prefix_set,
-            "/",
-            exp_set[j],
-            "/",
-            prefix_csv_input,
-            "_type_",
-            t,
-            ".csv"
-          ),
-          sep = ";"
-        )
-
+      csvr_df_file_name <- paste0(prefix_csv_input, "_", variations[j], ".csv")
+      csvr_file_path <- file.path(prefix_set, csvr_df_file_name)
+      csv_results_df <- read_delim(
+        file = csvr_file_path,
+        delim = ";"
+      )
+      
+      csv_results_df <- csv_results_df %>%
+        filter(type == paste0("t", t))
+      
+      csv_results_df <- csv_results_df %>%
+        rename_with(~ sub("(_grb|_hx)+$", "", .x))
+      
       csv_results_df <- csv_results_df %>%
         rename(obj_value = any_of("objValue"))
       
@@ -47,21 +41,22 @@ for (i in seq_along(solvers)){
           "Time (s)" = mean(time[found_sol == 1], na.rm = T),
         )
       
+      grouped_avrg_file_name <- paste0(
+        prefix_csv_output,
+        "_",
+        suff_output,
+        ".csv"
+      )
+      grouped_avrg_file_path <- file.path(
+        prefix_set,
+        "grouped_avrg",
+        grouped_avrg_file_name
+      )
+      dir.create(dirname(grouped_avrg_file_path), showWarnings = FALSE, recursive = TRUE)
+
       write.table(
         grouped_avrg,
-        paste0(
-          "../",
-          benchmarks[j],
-          "/",
-          prefix_set,
-          "/",
-          exp_set[j],
-          "/",
-          prefix_csv_output,
-          "_",
-          suff_output,
-          ".csv"
-        ),
+        grouped_avrg_file_path,
         sep = ";",
         dec = ".",
         quote = F,
