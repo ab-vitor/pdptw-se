cb2d897ee104db17a1d600657c704c813b7e6f6e
vabarbosa <vitor.alves@mindsight.com.br>
2026-01-08 23:41:35 -0300

    feat: initial bash for small inst gen island ams

---
+++ instances/bash_small_inst_gen_island_ams.sh
@@ -0,0 +1,17 @@
+mf=$1
+
+python3.9 small_inst_gen_islands.py --req 6 --vehi 6 --isl 2 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
+
+python3.9 small_inst_gen_islands.py --req 6 --vehi 6 --isl 4 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
+
+python3.9 small_inst_gen_islands.py --req 8 --vehi 8 --isl 2 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
+
+python3.9 small_inst_gen_islands.py --req 8 --vehi 8 --isl 4 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
+
+python3.9 small_inst_gen_islands.py --req 10 --vehi 10 --isl 2 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
+
+python3.9 small_inst_gen_islands.py --req 10 --vehi 10 --isl 4 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
+
+python3.9 small_inst_gen_islands.py --req 12 --vehi 12 --isl 2 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
+
+python3.9 small_inst_gen_islands.py --req 12 --vehi 12 --isl 4 --mach 4 --min_mach 3 --var_cap 20 --mach_spd 1 --ams --mf ${mf}
