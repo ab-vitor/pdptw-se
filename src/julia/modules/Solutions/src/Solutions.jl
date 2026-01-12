d2d013ab411b2be719ab94d2c02da830e49c3365
vabarbosa <vitor.alves@mindsight.com.br>
2026-01-12 19:48:36 -0300

    refactor: LP Solution data structures

--- src/julia/modules/Solutions/src/Solutions.jl
+++ src/julia/modules/Solutions/src/Solutions.jl
@@ -7,14 +7,14 @@ using Dates
 
 export VehicleStop, MachineTravel, StatsSolution, Solution
 
-export MIPVarsSolution, MIPStats, MIPSolution, LPSolution
+export LPSolution
 
 export createSolutionMelo, saveSolutionToFile, saveSolutionTimeline,
 	printDetailMeloFormulationSolution, validateSolution
 
 
 include("solutionDataStructures.jl")
-include("MIPSolutionDataStructures.jl")
+include("LPSolutionDataStructures.jl")
 include("printDetailed.jl")
 include("validateSolution.jl")
 include("statistics.jl")
