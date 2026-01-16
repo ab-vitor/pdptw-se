576ef021deca846a2f7dc1e7c4608d0eba2ba856
vabarbosa <vitor.alves@mindsight.com.br>
2026-01-16 17:26:03 -0300

    feat: init solution GreedyHeuristicMutate

--- src/julia/modules/GreedyHeuristicMutate/src/init_solution.jl
+++ src/julia/modules/GreedyHeuristicMutate/src/init_solution.jl
@@ -1,43 +1,33 @@
-function tightest_time_windows(inst::InstanceData)
+function tightest_time_windows(inst::InstanceData)::Vector{Int64}
 	reqs = copy(inst.V_p)
 	sort!(reqs, by = i -> (inst.jobs[inst.refs[i]].lat - inst.jobs[inst.refs[i]].earl))
 
 	return reqs
 end # function tightest_time_windows()
 
-function earliest_time_windows(inst::InstanceData)
-	reqs = copy(inst.V_p)
-	sort!(reqs, by = i -> inst.jobs[inst.refs[i]].lat)
-
-end # function earliest_time_windows()
-
-function init_vehicle_routes(inst::InstanceData)
+function init_vehicle_routes(inst::InstanceData)::Vector{Vector{VehicleStop}}
 	# Start depot -> 1 in inst.Vprime, which is equivalent to 0 in paper
-	firstVehicleStop = VehicleStop(1, inst.jobs[inst.refs[1]], 0, 0, 0, 0)
+	first_vehicle_stop = VehicleStop(1, inst.jobs[inst.refs[1]], 0, 0, 0, 0)
 	# End depot -> 2*inst.n+2 in inst.Vprime, which is equivalent to 2*n+1 in paper
-	lastVehicleStop = VehicleStop(2 * inst.n + 2, inst.jobs[inst.refs[2*inst.n+2]], 0, 0, 0, 0)
+	last_vehicle_stop = VehicleStop(2 * inst.n + 2, inst.jobs[inst.refs[2*inst.n+2]], 0, 0, 0, 0)
 
-	vehicleRoutes = Vector[VehicleStop[copy(firstVehicleStop), copy(lastVehicleStop)] for _ in inst.K]
-
-	return vehicleRoutes
+	vehicle_routes = Vector[VehicleStop[copy(first_vehicle_stop), copy(last_vehicle_stop)] for _ in inst.K]
+	return vehicle_routes
 end # function init_vehicle_routes()
 
 function init_machine_travels(inst::InstanceData)
 	# Insert a dummy MachineTravel in each machine travels list with start time at 0 (code simplification)
-	dummyMachineTravel = MachineTravel(0, 0, 0, 0, 0.0, true)
-	machineRoutes = Vector[MachineTravel[copy(dummyMachineTravel), copy(dummyMachineTravel)] for _ in inst.H]
+	dummy_machine_travel = MachineTravel(0, 0, 0, 0, 0.0, true)
+	machineRoutes = Vector[MachineTravel[copy(dummy_machine_travel), copy(dummy_machine_travel)] for _ in inst.H]
 
 	return machineRoutes
 end # function init_machine_travels()
 
-function get_service_order(inst::InstanceData, params::ParameterData)
+function get_service_order(inst::InstanceData, params::ParameterData)::Vector{Int64}
 	if params.greedy_service_order == "tightest_tw"
 		return tightest_time_windows(inst)
-	elseif params.greedy_service_order == "earliest_tw"
-		return earliest_time_windows(inst)
 	end
-
-	return inst.V_p # just in case
+	error("Service order $(params.greedy_service_order) not implemented.")
 end # function get_service_order()
 
 function init_solution(inst::InstanceData)
